c6409e4ec7c37c3791aed1aeea49c92532535dfc
SeitansPilgrim <90258714+SeitansPilgrim@users.noreply.github.com>
2022-02-16 11:09:04 +0100

    fixed navigation to peters db views
    for testing db

--- web-application/src/presentation-layer/routers/various-router.js
+++ web-application/src/presentation-layer/routers/various-router.js
@@ -14,4 +14,55 @@ router.get("/contact", function(request, response){
 	response.render("contact.hbs")
 })
 
+router.get("/accountCreate", function(request, response){
+	response.render("accountCreate.hbs")
+})
+
+router.get("/ad", function(request, response){
+	response.render("ad.hbs")
+})
+
+router.get("/adCreate", function(request, response){
+	response.render("adCreate.hbs")
+})
+
+router.get("/adDelete", function(request, response){
+	response.render("adDelete.hbs")
+})
+
+router.get("/ads", function(request, response){
+	response.render("ads.hbs")
+})
+
+router.get("/adUpdate", function(request, response){
+	response.render("adUpdate.hbs")
+})
+
+router.get("/myAds", function(request, response){
+	response.render("myAds.hbs")
+})
+
+router.get("/myBids", function(request, response){
+	response.render("myBids.hbs")
+})
+
+router.get("/c", function(request, response){
+	response.render("contact.hbs")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router
