113c41488860f3299db4342552c5e3836914a426
Magoulo <83534780+Magoulo@users.noreply.github.com>
2022-02-26 17:34:44 +0100

    data is marked up for create ad
    Close #43

--- web-application/src/presentation-layer/routers/ad-router.js
+++ web-application/src/presentation-layer/routers/ad-router.js
@@ -140,6 +140,15 @@ router.get('/myAds/:userID', function (request, response) {
         response.render("myAdBids.hbs", model)
     })
 })
+router.get("/adCreate", function (request, response) {
+    const model = {
+        session: request.session
+    }
+    response.render("adCreate.hbs", model)
+})
+router.post("/adCreate", function (request, response) {
+    response.render("adCreate.hbs")
+})
 
 router.get('/:adID', function (request, response) {
     const adID = request.params.adID
@@ -165,9 +174,6 @@ router.get("/ad", function (request, response) {
     response.render("ad.hbs", model)
 })
 
-router.get("/adCreate", function (request, response) {
-    response.render("adCreate.hbs")
-})
 
 
 
