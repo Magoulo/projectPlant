1f92f4a066e4de522c54a0d6ba0b8a8ef65b4a65
Sabina <90258714+SeitansPilgrim@users.noreply.github.com>
2022-02-26 13:54:14 +0100

    divided bid resource into own files

--- web-application/src/data-access-layer/ad-repository.js
+++ web-application/src/data-access-layer/ad-repository.js
@@ -69,47 +69,6 @@ exports.getImageBundleByAdID = function(adID, callback){
 	})
 }
 
-// ---------------- GET BID -----------------------------------------------
-
-exports.getAllBidsByAdID = function(adID, callback){
-	const query = "SELECT * FROM Bid WHERE adID = ?"
-	const values = [adID]
-
-	db.query(query, values, function(error, Bid){
-		if(error){
-			callback(['databaseError in Bid table'], null)
-		}else{
-			callback([], Bid)
-		}
-	})
-}
-
-exports.getBidByAdID = function(adID, callback){
-	const query = "SELECT * FROM Bid WHERE adID = ? LIMIT 1"
-	const values = [adID]
-
-	db.query(query, values, function(error, Bid){
-		if(error){
-			callback(['databaseError in Bid table'], null)
-		}else{
-			callback([], Bid[0])
-		}
-	})
-}
-
-exports.getAllBidsByUserID = function(userID, callback){
-	const query = "SELECT Bid.userID, Bid.adID, Bid.message, Bid.date, Bid.imagePath, Ad.title, Ad.latinName, Ad.description, ImageBundle.coverImagePath, ImageBundle.firstImagePath, ImageBundle.secondImagePath FROM Bid JOIN Ad ON Bid.adID = Ad.adID JOIN ImageBundle ON Ad.adID = ImageBundle.adID WHERE Bid.userID = ?"
-	const values = [userID]
-
-	db.query(query, values, function(error, Bid){
-		if(error){
-			callback(['databaseError in Bid table'], null)
-		}else{
-			callback([], Bid)
-		}
-	})
-}
-
 //----------------------- GET USER -------------------------------------------------------------------
 exports.getUserByUserID = function(userID, callback){
 	const query = "SELECT * FROM User WHERE userID = ? LIMIT 1"
