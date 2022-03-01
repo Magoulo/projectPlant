91037480cef1bbbcb0bb75370c28c74d7f43df51
Magoulo <83534780+Magoulo@users.noreply.github.com>
2022-03-01 11:57:21 +0100

    all images displayed on website is now stored in the images folder

--- database/initialize-database.sql
+++ database/initialize-database.sql
@@ -69,21 +69,28 @@ CREATE TABLE IF NOT EXISTS Bid (
 INSERT INTO UserAccount (username, passwordHash) VALUES ("moo", "123");
 INSERT INTO User (userAccountID,firstName,lastName,email,phoneNumber,city) VALUES ("1","Sabin","mooo","Sabimooon@com","555-123","GBG");
 INSERT INTO Ad (userID, title, latinName, description, isClosed) VALUES ("1","well maintained Monstera", "Monstera deliciosa","well maintained with roots in mullis", "0");
-INSERT INTO ImageBundle (adID, coverImagePath, firstImagePath, secondImagePath) VALUES ("1","https://blombudsportalen.se/wp-content/uploads/2021/04/Monstera-deliciosa-variegata.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR43qDr4ntXQXIUHKuvLkMB_VE6Jcuvn9zcPTTK2hMjjQ3bnuNp30yAHyiCHAE2t_hl0uo&usqp=CAU", "https://www.blomsterlandet.se/optimized/c-ProductDetailsSingle-Default-Sm2x/2293e09b/globalassets/catalog-images/7/43540/4c1dbcc6ea7fcfde28bafcd105ec647a.jpg");
+INSERT INTO ImageBundle (adID, coverImagePath, firstImagePath, secondImagePath) VALUES ("1","variegata-1.jpg", "monstera-propagation-in-water-scaled.jpg", "catesthill-propogating-monstera-plant-4.jpg");
+
+INSERT INTO Ad (userID, title, latinName, description, isClosed) VALUES ("1","Stunning Begonias", "Begonia maculata","Tradeing cuttings with good root development", "0");
+INSERT INTO ImageBundle (adID, coverImagePath, firstImagePath, secondImagePath) VALUES ("2","9dbis4pt2o751.jpg", "maculata-4-768x1024.jpg", "PXL_20210501_190619891.jpg");
+
 
 INSERT INTO UserAccount (username, passwordHash) VALUES ("rita", "123");
 INSERT INTO User (userAccountID,firstName,lastName,email,phoneNumber,city) VALUES ("2","Rita","Muggeldottir","RitaMuggeldottir@com","555-00","Stchaan");
 INSERT INTO Ad (userID, title, latinName, description, isClosed) VALUES ("2","Cashtree", "Pachira aquatica","Moneytree that keeps droppin bills", "0");
-INSERT INTO ImageBundle (adID, coverImagePath, firstImagePath, secondImagePath) VALUES ("2","https://www.mydomaine.com/thmb/PunwYf9El_Nyens5QMJ4d4moXOw=/1414x2121/filters:fill(auto,1)/GettyImages-1060620362-2940035374b843f3af034d0512dd2b54.jpg", "https://www.gardeningknowhow.com/wp-content/uploads/2015/04/money-tree.jpg", "https://www.gardeningknowhow.com/wp-content/uploads/2013/04/money-tree.jpg");
+INSERT INTO ImageBundle (adID, coverImagePath, firstImagePath, secondImagePath) VALUES ("3","krukvaxter_images_bilder_pachira_aquatica.jpg", "il_300x300.1012912306_dpeg.jpg", "strandkastanj-538318.jpg");
+
 
 INSERT INTO UserAccount (username, passwordHash) VALUES ("slugger", "123");
 INSERT INTO User (userAccountID,firstName,lastName,email,phoneNumber,city) VALUES ("3","Slugs","Slugsen","SluggiSlug@com","555-0222","JKPG");
-INSERT INTO Ad (userID, title, latinName, description, isClosed) VALUES ("3","Lilly", "blablaLatin","Lilly that keeps droppin bills", "0");
-INSERT INTO ImageBundle (adID, coverImagePath, firstImagePath, secondImagePath) VALUES ("3","https://i.pinimg.com/564x/aa/18/4e/aa184e45a9873944816d9ce825fb5a9a.jpg", "https://sunnyside-gardens.com/wp-content/uploads/2012/11/liliumstargazer.jpg", "https://www.americanmeadows.com/media/catalog/product/l/i/lilium-orientalis-oriental-lily-pink-stargazer-garden_2.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=560&width=560&canvas=560:560");
+INSERT INTO Ad (userID, title, latinName, description, isClosed) VALUES ("3","Stargazer lily", "Lilium orientalis 'Stargazer'","Flowers for both outdoors and indoors!", "0");
+INSERT INTO ImageBundle (adID, coverImagePath, firstImagePath, secondImagePath) VALUES ("4","aa184e45a9873944816d9ce825fb5a9a.jpg", "liliumstargazer.jpg", "lilium-orientalis-oriental-lily-pink-stargazer-garden_2.jpg");
+
 
 INSERT INTO UserAccount (username, passwordHash) VALUES ("qwe", "123");
 INSERT INTO User (userAccountID,firstName,lastName,email,phoneNumber,city) VALUES ("4","qwe","qwe","qweg@com","555-0222","qwecity");
 
+/*
 INSERT INTO Ad (userID, title, latinName, description, isClosed) VALUES ("1","Monstera", "Monstera deliciosa","Another Monstera", "0");
 INSERT INTO ImageBundle (adID, coverImagePath, firstImagePath, secondImagePath) VALUES ("4","https://www.mydomaine.com/thmb/PunwYf9El_Nyens5QMJ4d4moXOw=/1414x2121/filters:fill(auto,1)/GettyImages-1060620362-2940035374b843f3af034d0512dd2b54.jpg", "https://www.gardeningknowhow.com/wp-content/uploads/2015/04/money-tree.jpg", "https://www.gardeningknowhow.com/wp-content/uploads/2013/04/money-tree.jpg");
 INSERT INTO Ad (userID, title, latinName, description, isClosed) VALUES ("3","flower", "blablaLatin","Lilly that keeps droppin flowers", "0");
@@ -94,3 +101,4 @@ INSERT INTO Bid(userID, adID, date, imagePath, message, status) VALUES ("1", "2"
 INSERT INTO Bid(userID, adID, date, imagePath, message, status) VALUES ("3", "1", "2022-02-22", "This is the imagePath to bid", "I am takeing the monstera with me","Pending");
 INSERT INTO Bid(userID, adID, date, imagePath, message, status) VALUES ("1", "4", "2022-02-11", "This is the imagePath to bid", "I wanna trade that for this","Pending");
 INSERT INTO Bid(userID, adID, date, imagePath, message, status) VALUES ("1", "3", "2022-02-27", "This is the imagePath to bid", "Sling som lillies my way","Pending");
+*/
