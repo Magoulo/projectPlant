a3080147b632c5139258e5ae378b8ffd8bb31482
Sabina <90258714+SeitansPilgrim@users.noreply.github.com>
2022-03-06 11:09:47 +0100

    installed awilix
    with peters example code in app.js

--- web-application/src/presentation-layer/app.js
+++ web-application/src/presentation-layer/app.js
@@ -4,9 +4,31 @@ const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
 const fileUpload = require('express-fileupload')
-
 const session = require('express-session')
+
 let RedisStore = require("connect-redis")(session)
+
+//awilix setup---------------------------------------------------------------------
+const awilix = require('awilix')
+
+/*
+// Import the ones we want to use (real or mockup), real in this case.
+const accountRepository = require('data-access-layer/account-repository')
+const accountManager = require('business-logic-layer/account-manager')
+const accountRouter = require('presentation-layer/account-router')
+
+// Create a container and add the dependencies we want to use.
+const container = awilix.createContainer()
+container.register("accountRepository", awilix.asFunction(accountRepository))
+container.register("accountManager", awilix.asFunction(accountManager))
+container.register("accountRouter", awilix.asFunction(accountRouter))
+
+// Retrieve the router, which resolves all other dependencies.
+const theAccountRouter = container.resolve("accountRouter")  */
+
+//\awilix setup-----------------------------------------------------------------------
+
+
 
 // redis@v4
 const redis = require("redis")
