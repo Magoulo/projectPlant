69ba26161081056bf55e0087d1f3e7adde114db3
Magoulo <83534780+Magoulo@users.noreply.github.com>
2022-03-06 18:05:22 +0100

    added postfgres dependency

--- web-application/src/dal-sequelize/user-repository.js
+++ web-application/src/dal-sequelize/user-repository.js
@@ -1,10 +1,41 @@
 const { Sequelize, DataTypes } = require('sequelize')
 const sequelize = new Sequelize("postgres://user:pass@example.com:5432/dbname")
 
-const user = sequelize.define("user",{
-
+const User = sequelize.define("user", {
+	firstName: DataTypes.TEXT,
+	lastName: DataTypes.TEXT,
+	email: DataTypes.TEXT,
+	phoneNumber: DataTypes.TEXT,
+	city: DataTypes.TEXT
 })
+const UserAccount = sequelize.define("userAccount",{
+	username: DataTypes.TEXT,
+	passwordHash: DataTypes.TEXT
+})
+User.belongsTo(UserAccount)
 
+//User.sync()
+
+/*
+CREATE TABLE IF NOT EXISTS UserAccount (
+  userAccountID INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(45) NOT NULL,
+  passwordHash VARCHAR(60) NOT NULL,
+  PRIMARY KEY (userAccountID));
+
+CREATE TABLE IF NOT EXISTS User (
+  userID INT NOT NULL AUTO_INCREMENT,
+  userAccountID INT NOT NULL,
+  firstName VARCHAR(45) NULL,
+  lastName VARCHAR(45) NULL,
+  email VARCHAR(45) NULL,
+  phoneNumber VARCHAR(45) NULL,
+  city VARCHAR(45) NULL,
+   PRIMARY KEY (userID),
+  FOREIGN KEY (userAccountID)
+	REFERENCES UserAccount (userAccountID)
+	ON DELETE CASCADE);
+*/
 // -------------------- GET USER -----------------------------------
 
 exports.getUserByAccountID = function (userAccountID, callback) {
@@ -22,7 +53,7 @@ exports.getUserByAccountID = function (userAccountID, callback) {
 }
 
 exports.getUserByUserID = function (userID, callback) {
-	
+
 	const query = "SELECT * FROM User WHERE userID = ? LIMIT 1"
 	const values = [userID]
 
@@ -35,16 +66,16 @@ exports.getUserByUserID = function (userID, callback) {
 	})
 }
 
-exports.updateUserByUserID = function( userID, firstName, lastName, email, phoneNumber, city, callback){
+exports.updateUserByUserID = function (userID, firstName, lastName, email, phoneNumber, city, callback) {
 	const query = "UPDATE User SET firstName = ?, lastName = ?, email = ? , phoneNumber = ?, city = ? WHERE userID = ?"
-	const values = [firstName, lastName, email, phoneNumber, city, userID ]
+	const values = [firstName, lastName, email, phoneNumber, city, userID]
 
-	db.query(query, values, function(error){
-	callback(error)
+	db.query(query, values, function (error) {
+		callback(error)
 	})
 }
 
-exports.createUser = function(user,callback){
+exports.createUser = function (user, callback) {
 	const query = `INSERT INTO User (userAccountID, firstName, lastName, email, phoneNumber, city) VALUES (?, ?, ?, ?, ?, ?)`
 	const values = [user.userAccountID, user.firstName, user.lastName, user.email, user.phoneNumber, user.city]
 
